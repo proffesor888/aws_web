3d7f7c157ce88deff0b010ea581a51161415f1cc
proffesor888 <proffesor888@gmail.com>
2024-10-19 22:51:37 +0200

    Task 2 (#1)
    * infra added
    
    * links added
    
    ---------
    
    Co-authored-by: Anton Romanenko <anton_romanenko@epam.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 
+const filesNeedToExclude = ["infra/*"];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -13,6 +15,14 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    outDir: path.join(__dirname, "resources", "build"),
+    rollupOptions: {
+      external: [
+        ...filesNeedToExclude
+      ],
+    },
+  }, 
   test: {
     globals: true,
     environment: "jsdom",
