4fd33e3cbda8797881577f196a61bb463195048b
Anton Romanenko <anton_romanenko@epam.com>
2024-11-02 02:03:37 +0100

    pre-signed url generated

--- infra/lib/lambda/importProductsFile.ts
+++ infra/lib/lambda/importProductsFile.ts
@@ -1,21 +1,37 @@
 import { APIGatewayEvent, Handler } from "aws-lambda";
-import * as AWS from "aws-sdk";
 import { Readable } from "stream";
 import * as csv from "csv-parser";
+import * as AWS from "aws-sdk";
 
 const s3 = new AWS.S3();
 // const s3 = new aws_s3.Bucket();
 
 interface EventByFileName extends APIGatewayEvent {
   filename: string;
+  host: string;
+  path: string;
+  stage: string;
 }
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+};
+
 export const importProductsFile: Handler = async (event: EventByFileName) => {
-  const { filename = "" } = event;
-  if (filename.length) {
-    return { message: `upload/${filename}` };
+  const { filename = "", host, path, stage } = event;
+  if (filename.length && host && path && stage) {
+    const params = {
+      Bucket: "servicenkjsfngjknsrjktejt535",
+      Key: filename,
+    };
+    const signedURL = s3.getSignedUrl("putObject", params);
+    return {
+      headers,
+      url: signedURL,
+      filename: `uploaded/${filename}`,
+    };
   }
-  return { message: "test" };
+  return { headers, message: "data url missing" };
 };
 
 export const importFileParser: Handler = async (event) => {
@@ -36,13 +52,15 @@ export const importFileParser: Handler = async (event) => {
         .on("end", () => console.log(results));
     }
     return {
+      headers,
       statusCode: 200,
       body: "Success",
     };
   } catch (e) {
     return {
+      headers,
       statusCode: 500,
-      body: "Failed",
+      body: { error: e, message: "Failed", params },
     };
   }
 };
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,11 +1,12 @@
 const ID = "a0r5eyhpk7";
+const BUCKET_ID = "xpr49p16dh";
 const REGION = "us-east-1";
 const ENV = "prod";
 
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: `https://${BUCKET_ID}.execute-api.${REGION}.amazonaws.com/${ENV}`,
   bff: `https://${ID}.execute-api.${REGION}.amazonaws.com/${ENV}`,
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
